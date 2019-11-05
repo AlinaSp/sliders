39ab651fd862c84687ed8f5a4f2990fa2ad3b509
AlinaSp <alinasapiannik@gmail.com>
2019-11-05 20:45:51 +0200

    second slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 $('.slider-img').slick();
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  })
+});
